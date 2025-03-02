5ecfc76f53b39909b29734f3150759ae7f0d9f4f
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-03-02 00:05:05 -0500

    Changed the width of the filters section and added conditional rendering for the cards

--- src/components/client/DataStories.tsx
+++ src/components/client/DataStories.tsx
@@ -17,14 +17,14 @@ export default function DataStoriesComponent({ questionsList, filters }: any) {
 
     return (
         <div className="flex flex-row gap-8">
-            <section>
+            <section style={{ width: "25rem" }}>
                 <button className="w-full flex flex-wrap gap-3 p-4" onClick={handleClearFilters}>Clear filter</button>
                 <FiltersBar filtersList={filters} onFilterSelect={setSelectedFilter}></FiltersBar>
             </section>
             <section>
                 <div className="my-6 flex flex-row flex-wrap gap-4">
                     {
-                        filteredQuestions.map((listItem: DataStoryProps) => {
+                        filteredQuestions.length === 0 ? <h3>No Results Found.</h3> : filteredQuestions.map((listItem: DataStoryProps) => {
                             return (
                                 <DataStory key={listItem.question} question={listItem.question} answer={listItem.answer}></DataStory>
                             )
