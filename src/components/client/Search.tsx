439879e1aab1af10f24ff3640ecc3cb55485fa5e
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-03-09 22:39:25 -0400

    Added Search powered by Algolia below the search box

--- src/components/client/Search.tsx
+++ src/components/client/Search.tsx
@@ -23,13 +23,16 @@ function Hit({ hit }: any) {
 export default function SearchComponent() {
     return (
         <InstantSearch searchClient={searchClient} indexName="articlesIndex">
-            <div className="relative my-5">
+            <div className="relative flex flex-col gap-5 my-5">
                 <SearchBox classNames={{
                     root: "relative w-full max-w-lg",
                     input: "w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-blue-500",
                     submitIcon: "hidden",
                     resetIcon: "hidden",
                 }} placeholder="Search articles..." />
+                <div className="flex flex-row justify-end">
+                    <p className="text-zinc-500 text-1xl mb-9 text-wrap">Search powered by: <img src="/Algolia logo.png" className="inline" alt="Algolia Logo" width="5%" height="5%" /></p>
+                </div>
             </div>
             <section className="flex flex-col gap-3 mb-10 mx-auto">
                 <h2 className='text-2xl text-purple-500 text-left font-bold my-2 leading-relaxed'>Results:</h2>
