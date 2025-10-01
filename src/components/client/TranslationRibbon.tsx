580ff9b88d52438e3bb1c4fad4ddb529243b5082
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-09-30 23:57:51 -0400

    Changed the CSS classes for the buttons

--- src/components/client/TranslationRibbon.tsx
+++ src/components/client/TranslationRibbon.tsx
@@ -2,25 +2,42 @@ import { useState } from "react";
 import TranslateButton from "./TranslateButton.tsx";
 
 interface TranslationRibbonProps {
-    translateFunction: (language: string) => void;
+  translateFunction: (language: string) => void;
 }
 
-export default function TranslationRibbon({ translateFunction }: TranslationRibbonProps) {
-    const [isTranslating, setIsTranslating] = useState(false);
+export default function TranslationRibbon({
+  translateFunction,
+}: TranslationRibbonProps) {
+  const [isTranslating, setIsTranslating] = useState(false);
 
-    async function handleTranslate(language: string) {
-        setIsTranslating(true);
-        await translateFunction(language);
-        setIsTranslating(false);
-    }
+  async function handleTranslate(language: string) {
+    setIsTranslating(true);
+    await translateFunction(language);
+    setIsTranslating(false);
+  }
 
-    return (
-        <section id="tranlation-section">
-            <div className="flex gap-7 overflow-x-auto">
-                <TranslateButton className="inline-block px-5 py-2.5 rounded-md bg-teal-900 text-white font-semibold shadow-md hover:bg-gray-500 transition duration-200" targetLanguage="French" translationFunction={handleTranslate} isTranslating={isTranslating} />
-                <TranslateButton className="inline-block px-5 py-2.5 rounded-md bg-teal-900 text-white font-semibold shadow-md hover:bg-gray-500 transition duration-200" targetLanguage="Spanish" translationFunction={handleTranslate} isTranslating={isTranslating} />
-                <TranslateButton className="inline-block px-5 py-2.5 rounded-md bg-teal-900 text-white font-semibold shadow-md hover:bg-gray-500 transition duration-200" targetLanguage="Japanese" translationFunction={handleTranslate} isTranslating={isTranslating} />
-            </div>
-        </section>
-    )
+  return (
+    <section id="tranlation-section">
+      <div className="flex gap-7 overflow-x-auto">
+        <TranslateButton
+          className="relative inline-flex items-center px-5 py-2 rounded-full font-medium text-white bg-teal-900 shadow-md border-2 border-transparent before:absolute before:inset-0 before:rounded-full before:p-[2px] my-6"
+          targetLanguage="French"
+          translationFunction={handleTranslate}
+          isTranslating={isTranslating}
+        />
+        <TranslateButton
+          className="relative inline-flex items-center px-5 py-2 rounded-full font-medium text-white bg-teal-900 shadow-md border-2 border-transparent before:absolute before:inset-0 before:rounded-full before:p-[2px] my-6"
+          targetLanguage="Spanish"
+          translationFunction={handleTranslate}
+          isTranslating={isTranslating}
+        />
+        <TranslateButton
+          className="relative inline-flex items-center px-5 py-2 rounded-full font-medium text-white bg-teal-900 shadow-md border-2 border-transparent before:absolute before:inset-0 before:rounded-full before:p-[2px] my-6"
+          targetLanguage="Japanese"
+          translationFunction={handleTranslate}
+          isTranslating={isTranslating}
+        />
+      </div>
+    </section>
+  );
 }
