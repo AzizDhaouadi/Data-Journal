c2fee8cc2d9d23959a52497e4949f1360d887219
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-10-11 15:21:11 -0400

    Removed the script that is supposed to show the button and added it to Layout.astro

--- src/scripts/axeptio.js
+++
@@ -1,8 +0,0 @@
-// Handle view transitions
-document.addEventListener("astro:page-load", () => {
-    console.log("Reloading Axeptio after navigation");
-    if (window.showAxeptioButton) {
-        console.log("Window method detected, reloading Axeptio button");
-        window.showAxeptioButton();
-    }
-});
