636737db122d2ff44e6f6c51eef6e1da6f662bc4
Aziz Dhaouadi <dhaziz.works@gmail.com>
2025-10-16 23:41:56 -0400

    Modified the style of the cards component in data stories

--- src/components/client/DataStory.tsx
+++ src/components/client/DataStory.tsx
@@ -8,7 +8,15 @@ export default function DataStory({
   if (question) {
     return (
       <>
-        <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white border border-gray-200 p-5">
+        <div
+          className="max-w-sm rounded-2xl overflow-hidden bg-white p-5"
+          style={{
+            border: "2px solid #f15a25",
+            cursor: "pointer",
+            boxShadow: "8px 8px #311c3b",
+            transition: "transform .2s ease, box-shadow .2s ease",
+          }}
+        >
           <div className="py-4">
             <h2 className="text-xl font-semibold font-spectral">{question}</h2>
             <p className="text-gray-600 mt-2">{answer}</p>
