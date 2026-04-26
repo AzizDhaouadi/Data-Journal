b6c6d2caacc7a35d3a5fa5160d9d3f21d4d67570
Aziz Dhaouadi <dhaziz.works@gmail.com>
2026-04-25 23:24:14 -0400

    Added two new props for the DataStory component

--- src/components/client/DataStory.tsx
+++ src/components/client/DataStory.tsx
@@ -1,21 +1,28 @@
 export default function DataStory({
   question,
   answer,
+  link,
+  description,
 }: {
   question: string;
-  answer: string;
+  answer: string | number;
+  link?: string;
+  description?: any;
 }) {
   if (question) {
     return (
       <>
         <div className="max-w-sm rounded-xl bg-white border border-zinc-900 p-5">
-          <div className="py-4 flex flex-col gap-3">
+          <div className="py-4 flex flex-row gap-3">
             <h2 className="font-manrope font-bold text-xl text-zinc-900 leading-snug">
-              {question}
+              <a href={link}>{question}</a>
             </h2>
             <p className="font-manrope text-sm text-zinc-500 leading-relaxed">
               {answer}
             </p>
+          </div>
+          <div>
+            <p>{description}</p>
           </div>
           <div className="w-8 h-1 bg-[#E8522A]" />
         </div>
